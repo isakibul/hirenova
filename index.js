5acabd658c52bc0f5443e8cb8cf30f5287c2e7af
isakibul <isakibul108@gmail.com>
2025-06-22 13:54:03 +0600

    initialize project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
